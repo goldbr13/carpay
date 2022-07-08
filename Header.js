7d5c2e5748e8ad1ca311fdfc7c235edd4a2a4505
Saboo, Pranav (ps529a) <ps529a@att.com>
2022-07-08 11:47:57 -0400

    Added logo image to header

--- Header.js
+++ Header.js
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar, Nav, Container, Row, Col, Button, Table } from 'react-bootstrap';
-
+import logo from './assets/flo_v8_logo.png';
 export function Header() {
     return(
     <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">
+            <img src={logo} width="80" height="60" alt="Flo Logo"></img>
+            Flo's V8 Cafe
+        </Navbar.Brand>
+        
         <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
