a4824d6ff8a02b7435dd983a39cd7cfc28f2faab
goldbr13 <goldbr13@tcnj.edu>
2022-07-15 12:49:48 -0400

    implementing the ability to add a new card

--- Modal.js
+++ Modal.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Modal, ListGroup, Nav, Container, Row, Col, Button, Table } from 'react-bootstrap';
 import { useState } from 'react';
-import { TextInput } from "react-native-web";
 
 export function PaymentModal(){
   const [show, setShow] = useState(false);
@@ -46,7 +45,27 @@ export function PaymentModal(){
             </ListGroup>
         </Modal.Body>
         <Modal.Footer>
-        <input></input>
+          <form>
+            <br></br>
+            <label for="card"> Enter card number: </label>
+            <input type="number" id="card"></input><br></br>
+            <label for="cvc"> CVC: </label>
+            <input type="number" id="cvc"></input> 
+            <br></br>
+            <label for="date"> Expiration Date: </label>
+            <input type="month" id="date"></input> 
+            <select id="types">
+              <option> Select Card Type </option>
+              <option value="Mastercard"> Mastercard </option>
+              <option value="Visa"> Visa </option>
+              <option value="American Express"> American Express </option>
+              <option value="Discover"> Discover </option>
+            </select>
+            <br></br>
+          </form>
+        
+          <br></br>
+
         <Button variant="primary" onClick={handleClose}>
             Add Card
         </Button>
